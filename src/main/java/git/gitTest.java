7eea03c6a7f8d4730f9f39966d825ea79b88db79
wt <wt@qq.com>
2022-07-09 02:40:57 +0800

    master1

---
+++ src/main/java/git/gitTest.java
@@ -0,0 +1,18 @@
+package git;
+
+public class gitTest{
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+        System.out.println("Hello World");
+        System.out.println("Hello Git3");
+        System.out.println("hot-fix test");
+        System.out.println("Hello Git4");
+        System.out.println("mastertest");
+        System.out.println("push");
+        System.out.println("push");
+        System.out.println("push");
+        System.out.println("push");
+        System.out.println("push");
+        System.out.println("push");
+    }
+}
